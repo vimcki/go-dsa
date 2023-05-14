5e99f217184d983898fce9021a5002074a9fd8e8
vimcki <0xvimcki@gmail.com>
2023-05-14 22:34:25 +0200

    add graph_search test cases

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/vimcki/go-dsa/internal/binarytree"
 	findMaxSubarray "github.com/vimcki/go-dsa/internal/find_max_subarray"
+	graphsearch "github.com/vimcki/go-dsa/internal/graph_search"
 	"github.com/vimcki/go-dsa/internal/hashmap"
 	linkedlist "github.com/vimcki/go-dsa/internal/linked_list"
 	priorityqueue "github.com/vimcki/go-dsa/internal/priority_queue"
@@ -22,6 +23,5 @@ func main() {
 	linkedlist.Test()
 	hashmap.Test()
 	binarytree.Test()
-	// binary tree
-	// red black tree
+	graphsearch.Test()
 }
--- internal/graph_search/check.go
+++ internal/graph_search/check.go
@@ -1,15 +1,73 @@
 package graphsearch
 
-import "github.com/vimcki/go-dsa/internal/graph_search/bfs"
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/vimcki/go-dsa/internal/graph_search/bfs"
+)
 
 type searcher interface {
 	Search(from, to int) ([]int, error)
 }
 
 func Test() {
-	b := bfs.New([][]bfs.Edge{})
-	test("bfs", b)
+	b := bfs.New([][]bfs.Edge{
+		{
+			{To: 1, Weight: 1},
+			{To: 3, Weight: 1},
+		},
+
+		{
+			{To: 2, Weight: 2},
+			{To: 4, Weight: 3},
+		},
+		{
+			{To: 5, Weight: 1},
+		},
+		{
+			{To: 8, Weight: 1},
+		},
+		{
+			{To: 3, Weight: 7},
+			{To: 8, Weight: 2},
+			{To: 5, Weight: 5},
+		},
+		{
+			{To: 6, Weight: 7},
+			{To: 10, Weight: 2},
+		},
+		{
+			{To: 7, Weight: 1},
+		},
+		{
+			{To: 6, Weight: 1},
+		},
+		{
+			{To: 9, Weight: 3},
+		},
+		{
+			{To: 11, Weight: 1},
+		},
+		{
+			{To: 9, Weight: 2},
+		},
+		{},
+	})
+	test("bfs from 0 to 5", b, 0, 5, []int{0, 1, 2, 5})
+	test("bfs from 1 to 6", b, 1, 11, []int{1, 2, 5, 10, 9, 11})
 }
 
-func test(name string, s searcher) {
+func test(name string, s searcher, from, to int, want []int) {
+	fmt.Printf("Running test: %s\n", name)
+	got, err := s.Search(from, to)
+	if err != nil {
+		fmt.Printf("Search returned error: %v\n", err)
+		return
+	}
+	if !reflect.DeepEqual(got, want) {
+		fmt.Printf("Search = %v, want %v\n", got, want)
+	} else {
+		fmt.Println("Test passed.")
+	}
 }
